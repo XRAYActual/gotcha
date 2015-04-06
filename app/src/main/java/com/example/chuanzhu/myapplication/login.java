580ac6d2995303a23d39bd3567191044790a88c1
xcz011 <chuanzhu,xu@gmail.com>
2015-04-06 14:55:30 -0400

    ddd

--- app/src/main/java/com/example/chuanzhu/myapplication/login.java
+++ app/src/main/java/com/example/chuanzhu/myapplication/login.java
@@ -7,11 +7,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 
 public class login extends ActionBarActivity {
 
-
+    EditText username;
 
 
 
